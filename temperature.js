6d76c59ed5dd2e2b61b5d8e2097f90fa2856ac4f
Rafaherrero <rafael.herrero.13@ull.edu.es>
2016-02-28 21:07:06 +0000

    Añadido patron en la rama gh-pages

--- temperature.js
+++ temperature.js
@@ -2,7 +2,7 @@
 function calculate() {
   var result;
   var temp = original.value;
-  var regexp = /\s*([-+]?\d+(?:\.\d*)?\s*(?:([eE][-+]?[0-9]+)?))\s*([fFcC]$|[fF][a]$|[fF][a][r]$|farenheit|Farenheit)/;
+  var regexp = /\s*([-+]?\d+(?:\.\d*)?\s*(?:([eE][-+]?[0-9]+)?))\s*([fFcC]\s*$|[fF][a]?[h]?[r]?[e]?[n]?[h]?[e]?[i]?[t]?\s*$|fahrenheit$|Fahrenheit$|[cC][e][l]?[s]?[i]?[u]?[s]?\s*$|celsius$|Celsius$)/;
 
   var m = temp.match(regexp);
 
